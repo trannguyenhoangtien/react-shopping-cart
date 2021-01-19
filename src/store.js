e34827c0ba0fcb1505ff2645384e2d1c3d3c7c08
Hoàng Tiến <hoangtien@huongnghiepaau.com>
2021-01-19 21:50:10 +0700

    create order

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
-import {productsReducer} from './reducers/productReducers'
+import {productsReducer} from './reducers/productReducers';
+import {orderReducer} from './reducers/orderReducers';
 
 const initialState = {};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(combineReducers({
         products: productsReducer,
-        cart: cartReducer
+        cart: cartReducer,
+        order: orderReducer,
     }),
     initialState,
     composeEnhancer(applyMiddleware(thunk))
