b41e4f64f37bfe0cefd9698a04c6dace15ff5afd
Hoàng Tiến <hoangtien@huongnghiepaau.com>
2021-02-02 15:39:34 +0700

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,8 @@
+const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+    const app = express();
   app.use(
     '/api',
     createProxyMiddleware({
