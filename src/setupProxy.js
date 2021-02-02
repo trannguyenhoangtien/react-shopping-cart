a133f7883cbe4358a1fdd5a55d55808f97dc4fc9
Hoàng Tiến <hoangtien@huongnghiepaau.com>
2021-02-02 15:50:15 +0700

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,10 @@
-const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    const app = express();
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://ht-react-shopping-cart.herokuapp.com:5000',
+      target: 'http://localhost:5000',
       changeOrigin: true,
     })
   );
