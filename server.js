fd1e851b78c90aaee383989f123081c0b2aa35d6
Hoàng Tiến <hoangtien@huongnghiepaau.com>
2021-02-02 15:14:23 +0700

    edit server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const shortid = require("shortid");
 const app = express();
 app.use(bodyParser.json());
 
+app.use("/", express.static(__dirname + "/build"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+
 mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
   useNewUrlParser: true,
   useCreateIndex: true,
